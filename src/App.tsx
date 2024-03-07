574650edc690de9110178289bf8dcb8bee4b6d3b
John Paul <113358424+Viadsss@users.noreply.github.com>
2024-03-07 22:30:30 +0800

    Init Commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import CokeBottle from "./CokeBottle";
+
+function App() {
+  return (
+    <div className="bg-slate-100 p-8 min-h-screen">
+      <div className="py-96 border">Wow</div>
+      <div className="max-h-52 h-full aspect-video bg-white">
+        <CokeBottle />
+      </div>
+      <div className="py-96 border">Wow</div>
+    </div>
+  );
+}
+
+export default App;
